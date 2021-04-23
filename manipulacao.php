97a09d98fa52b06f3a558851df3b48682d7d5dca
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-04-22 21:06:48 -0300

    chore: filter countries by space in the name

--- manipulacao.php
+++ manipulacao.php
@@ -20,12 +20,19 @@ $contador = count($dados);
 echo "Número de países: $contador\n";
 
 # Class 2
-function convertePaisPraLetraMaiuscula(array $pais)
+function convertePaisPraLetraMaiuscula(array $pais): array
 {
     $pais['pais'] = mb_convert_case($pais['pais'], MB_CASE_UPPER);
     return $pais;
-};
+}
 
-$arrayModificado = array_map('convertePaisPraLetraMaiuscula', $dados);
+function verificaSePaisTemEspacoNoNome(array $pais): bool
+{
+    return str_contains($pais['pais'], ' '); // PHP 8+
+    //return strpos($pais['pais'], ' ') !== false; // PHP 7.4
+}
 
-var_dump($arrayModificado);
+$dados = array_map('convertePaisPraLetraMaiuscula', $dados);
+$dados = array_filter($dados, 'verificaSePaisTemEspacoNoNome');
+
+var_dump($dados);
